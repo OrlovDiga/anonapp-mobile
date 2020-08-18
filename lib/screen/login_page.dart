32deaad0cd46d1244e1f83d8a11667d802619510
OrlovDiga <privaloffdim@yandex.ru>
2020-08-18 22:24:10 +0300

    add save token realization

--- lib/screen/login_page.dart
+++ lib/screen/login_page.dart
@@ -1,8 +1,13 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:anonapp_mobile/screen/registration_page.dart';
 import 'package:flutter/material.dart';
 import 'package:anonapp_mobile/animation/fade_animation.dart';
 import 'package:anonapp_mobile/screen/start_page.dart';
 import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'package:cookie_jar/cookie_jar.dart';
 
 class LoginPage extends StatelessWidget {
   @override
@@ -118,14 +123,75 @@ class LoginPage extends StatelessWidget {
                           borderRadius: BorderRadius.circular(10.0),
                         ),
                         onPressed: () async {
-                          var url = "http://localhost:8080/anonapp/api/register";
+                          var url = "http://127.0.0.1:8080/authenticate";//"http://localhost:8080/anonapp/api/authenticate";
 
+                          Map jsonData = {
+                            'username': 'user',
+                            'password': '1234'
+                          };
+
+//                          var cj = new CookieJar();
+//
+//                          HttpClientRequest request = await HttpClient().post(
+//                            url,
+//                            headers:
+//                          )
+//                          ..headers.contentType = ContentType.json
+//                          ..write(jsonData);
+                          print(jsonEncode(jsonData));
+                          http.post(url,
+                              headers: {"Content-Type": "application/json"},
+                              body: jsonEncode(jsonData),
+                          ).then((http.Response response) {
+//                            print("Response status: ${response.statusCode}");
+//                            print("Response body: ${response.contentLength}");
+//                            print(response.headers);
+//                            print(response.request);
+//                            print(response.headers['set-cookie']);
+                            var cookies = response.headers['set-cookie'];
+                            var cookiesList = cookies.split(';');
+                            print(cookiesList);
+                            cookiesList.forEach((e) => {
+                              if (e.contains("token=")) {
+                                print(e),
+                                File('/Users/macbook/AndroidStudioProjects/anonapp_mobile/assets/config/token').
+                                writeAsStringSync(e)
+                              }
+                            });
+                          });
+
+//                          HttpClientResponse response = await request.close();
+//                          final cont
+//
+//                          ents = StringBuffer();
+//                          final completer = Completer<String>();
+//                          response.transform(utf8.decoder).listen((data) {
+//                            contents.write(data);
+//                          }, onDone: () => completer.complete(contents.toString()));
+//                          print(completer);
+//                          print(contents);
+//                          print(response.statusCode);
+//                          print(response.cookies);
+//                          print(response.headers);
+                          //cj.saveFromResponse(Uri.parse(url), response.cookies);
+
+                          //List<Cookie> c = cj.loadForRequest(Uri.parse(url));
+                          //print(c);
+
+
+
+
+                          /*var cj = new CookieJar();
+                          List<Cookie> results = cj.loadForRequest(Uri.parse("http://localhost:8080/anonapp/api/authenticate"));
+                          
                           var response = await http.post(
                               url,
-                              body: {'login': 'doodle', 'password': 'blue'});
+                              body: {'login': 'lol', 'password': '1024'});
                           print('Response status: ${response.statusCode}');
                           print('Response body: ${response.body}');
+                          */
 
+                          //print(results);
                           Navigator.push(
                               context,
                               MaterialPageRoute(builder: (context) => StartPage()));
@@ -168,13 +234,43 @@ class LoginPage extends StatelessWidget {
   }
 
   Future<void> authentication() async {
-    var url = "http://localhost:8080/anonapp/api/register";
+    var url = "http://localhost:8080/anonapp/api/authenticate";
 
     var response = await http.post(
         url,
         body: {'login': 'doodle', 'password': 'blue'});
-    print('Response status: ${response.statusCode}');
+
+    //String token = ;
+    final file =  File('/Users/macbook/AndroidStudioProjects/anonapp_mobile/assets/config/token');
+    file.writeAsStringSync(response.headers['set-cookie']);
+    String rawCookie = response.headers['set-cookie'];
+    updateCookie(response);
+
+
+    print('cookie =  ${response.headers['set-cookie']}');
+    print('object');
+    print('Response status: ${response.statusCode} ${response.headers['set-cookie']}');
+    print('Response status: ${response.statusCode} ${response.headers['set-cookie']}');
     print('Response body: ${response.body}');
   }
 
+  /*Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.txt');
+  }*/
+
+  void updateCookie(http.Response response) {
+    Map<String, String> headers = {};
+
+    String rawCookie = response.headers['set-cookie'];
+    if (rawCookie != null) {
+      print('1');
+      int index = rawCookie.indexOf(';');
+      headers['cookie'] =
+      (index == -1) ? rawCookie : rawCookie.substring(0, index);
+    } else{
+      print('2');
+    }
+
+  }
 }
